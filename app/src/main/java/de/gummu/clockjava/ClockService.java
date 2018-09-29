7f5fa10e7fc49a1177eacceecf4c41d267cf0f17
Fabian Pflug <fabian.pflug@gummu.de>
2018-09-29 16:08:23 +0200

    A somewhat working version

--- app/src/main/java/de/gummu/clockjava/ClockService.java
+++ app/src/main/java/de/gummu/clockjava/ClockService.java
@@ -9,6 +9,11 @@ import android.preference.PreferenceManager;
 import android.util.Log;
 
 import org.eclipse.paho.android.service.MqttAndroidClient;
+import org.eclipse.paho.client.mqttv3.IMqttToken;
+import org.eclipse.paho.client.mqttv3.MqttConnectOptions;
+import org.eclipse.paho.client.mqttv3.MqttException;
+import org.eclipse.paho.client.mqttv3.MqttMessage;
+import org.eclipse.paho.client.mqttv3.persist.MemoryPersistence;
 
 import java.io.IOException;
 import java.text.DateFormat;
@@ -19,41 +24,57 @@ import java.util.TimeZone;
 
 public class ClockService extends JobService {
 
+    private static String TAG = ClockService.class.getCanonicalName();
+    private static final String DEFAULT_BROKER = "tcp://192.168.0.2:1883";
+    private static final String DEFAULT_CLIENT_ID = "AndroidClock";
+    private static final String DEFAULT_TOPIC = "/openhab/handy/test";
     public static final String MQTT_BROKER_URL = "mqttbroker";
     public static final String MQTT_CLIENT_ID = "mqttclient";
     public static final String MQTT_TOPIC = "mqtttopic";
-
-
-    private MqttAndroidClient client;
+    private MqttAndroidClient mqttClient;
+    private MqttConnectOptions connectionOptions;
+    private IMqttToken connectionToken;
 
 
     @Override
     public boolean onStartJob(final JobParameters params) {
-        final ClockService cs = this;
-        final MqqClient pahoMqttClient = new MqqClient();
 
+        final ClockService context = this;
+        SharedPreferences sharedPref = PreferenceManager.getDefaultSharedPreferences(context);
+        String broker   = sharedPref.getString(MQTT_BROKER_URL, DEFAULT_BROKER);
+        Log.d(TAG, "Broker: " + broker);
+        String clientid = sharedPref.getString(MQTT_CLIENT_ID, DEFAULT_CLIENT_ID);
+        Log.d(TAG, "ClientID: " + clientid);
+
+        mqttClient = new MqttAndroidClient(context, broker, clientid , new MemoryPersistence());
+        connectionOptions = new MqttConnectOptions();
+        mqttClient.registerResources(context);
         new Thread(new Runnable() {
             public void run() {
-                AlarmManager alarmMgr = (AlarmManager)getSystemService(Context.ALARM_SERVICE);
+                AlarmManager alarmMgr = (AlarmManager) context.getSystemService(Context.ALARM_SERVICE);
                 AlarmManager.AlarmClockInfo info = alarmMgr.getNextAlarmClock();
-                SharedPreferences sharedPref = PreferenceManager.getDefaultSharedPreferences(cs);
-                String broker   = sharedPref.getString(MQTT_BROKER_URL, "\"192.168.0.4\"");
-                Log.d("Sender", "Broker: " + broker);
-                String clientid = sharedPref.getString(MQTT_CLIENT_ID, "\"AndroidClock\"");
-                Log.d("Sender", "ClientID: " + clientid);
-                client = pahoMqttClient.getMqttClient(getApplicationContext(), broker, clientid);
-                String topic = sharedPref.getString(MQTT_TOPIC, "\"/openhab/phone/test\"");
-                Log.d("Sender", "Topic: " + topic);
-
-                    if(info!=null) {
-                        Date d = new Date(info.getTriggerTime());
-                        final DateFormat iso8601DateFormatter = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss'Z'", Locale.ENGLISH);
-                        iso8601DateFormatter.setTimeZone(TimeZone.getTimeZone("Europe/Berlin"));
-                        pahoMqttClient.publishMessage(client, iso8601DateFormatter.format(d), 1, topic);
-                    } else {
-                        pahoMqttClient.publishMessage(client, "UNDEFINED", 1, topic);
+                SharedPreferences sharedPref = PreferenceManager.getDefaultSharedPreferences(context);
+                String topic = sharedPref.getString(MQTT_TOPIC, DEFAULT_TOPIC);
+                Log.d(TAG, "Topic: " + topic);
+                if(! mqttClient.isConnected() ) {
+                    try {
+                        connect();
+                    }catch (Exception e) {
+                        Log.w(TAG, e);
+                        return;
                     }
-                    Log.d("Thread", "Finished Sending");
+                }
+
+                if(info!=null) {
+                    Date d = new Date(info.getTriggerTime());
+                    final DateFormat iso8601DateFormatter = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss'Z'", Locale.ENGLISH);
+                    iso8601DateFormatter.setTimeZone(TimeZone.getTimeZone("Europe/Berlin"));
+                    doSend(topic, iso8601DateFormatter.format(d));
+                } else {
+                    doSend(topic, "UNDEFINED");
+                }
+                Log.d(TAG, "Finished Sending");
+
 
                 jobFinished( params, false );
             }
@@ -64,6 +85,47 @@ public class ClockService extends JobService {
 
     @Override
     public boolean onStopJob(JobParameters params) {
+        Log.d(TAG, "Stopping Job");
         return false;
+    }
+
+
+    private void connect() throws MqttException {
+        if( mqttClient.isConnected()) return;
+        if(connectionToken != null) {
+            if(connectionToken.isComplete()) {
+                connectionToken = null;
+                return;
+            }
+        } else {
+            connectionToken = mqttClient.connect(connectionOptions);
+        }
+        if(connectionToken != null)
+            connectionToken.waitForCompletion(5000);
+    }
+
+
+    private void doSend(String topic, String msg) {
+        Log.d(TAG, "Sending message " + msg + " to topic " + topic);
+        MqttMessage message = new MqttMessage(msg.getBytes());
+        message.setQos( 1 );
+        try {
+            mqttClient.publish(topic, message);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void terminate() {
+        try {
+            disconnect();
+        } catch (MqttException e) {
+            e.printStackTrace();
+        }
+        mqttClient.unregisterResources();
+    }
+
+    public void disconnect() throws MqttException {
+        mqttClient.disconnect();
     }
 }
